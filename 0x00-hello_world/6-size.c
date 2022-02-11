8686451dd87c4f6ef727e6da1bcebbd54af5e3e7
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-10 18:29:14 -0800

    Imprimir el tamaño de variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int i1;
+	long long int i2;
+	float f;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of a int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(i1));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(i2));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(f);
+	return (0);
+}
