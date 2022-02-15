1e309d7a0b046fbdeee28b584f1bf50021662ea1
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-15 12:30:19 -0800

    Se agrega el task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+  #include "main.h"
+/**
+ * print_last_digit - check the code.
+ * @n: un entero de la funcion
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+
+	if ( n >= 0)
+	{
+		n = n % 10;
+		return (t);
+	}
+	else
+	{
+		n = n * -1;
+		return (n);
+	}
+	return (0);
+}
