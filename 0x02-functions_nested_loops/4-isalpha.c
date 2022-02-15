783e1e19406ab7ae0e23052501415dc7b9cd0086
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-15 08:13:32 -0800

    Se agrego el task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+ #include "main.h"
+ #include <ctype.h>
+ #include <stdio.h>
+/**
+ * _islower - check the code.
+ * @c: un entero de la funcion
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
