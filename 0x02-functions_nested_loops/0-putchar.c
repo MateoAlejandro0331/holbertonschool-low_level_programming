a4ff646fbaeec9cfea524524e6f7732711291ecb
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-14 20:35:53 -0500

    Agregamos todas las carpetas

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
@@ -8,7 +8,13 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char c [] = "_putchar";
 
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');ifndef
 	return (0);
 }
