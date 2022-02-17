01252da35f3ae0f6fc2f64ee3d96cfd9fcb98d78
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-17 17:34:45 -0500

    Ojala funcione

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > c && a > b)
+if (a >= c && a >= b)
 {
 largest = a;
 }
-else if (b > a && b > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
-else if (c > a && c > b)
+else if (c >= a && c >= b)
 {
 largest = c;
 }
