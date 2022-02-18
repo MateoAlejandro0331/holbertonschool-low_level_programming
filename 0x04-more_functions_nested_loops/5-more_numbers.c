04a190f817fe88dc823c10970fed4d87036f51b3
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-18 16:43:44 -0500

    Se agrego task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int filas;
+
+	for (filas = 0; filas <= 10; filas++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num <= 9)
+			{
+				_putchar(num +48);
+			}
+			else
+			{
+				_putchar((num/10) + 48);
+				_putchar((num%10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
