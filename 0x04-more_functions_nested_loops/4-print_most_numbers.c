522be8628f941c6ca0e6a8466550ff41a86baef7
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-18 16:02:16 -0500

    Se creo el task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num != '2' && num != '4')
+		{
+			_putchar(num);
+		}
+	}
+	_putchar('\n');
+}
