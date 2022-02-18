b79853cf51e9e94866c3807a51cd81ee83e3af94
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-18 17:13:49 -0500

    Se agrego el task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * more_numbers - check the code
  *
  * Return: Always 0.
  */
 
 void more_numbers(void)
 {
-	char numeros[] = {0,1,2,3,4,5,6,7,8,9,1,0,1,1,1,2,1,3,1,4};
-	int columnas;
-	int filas;
+char numeros[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, 1, 2, 1, 3, 1, 4};
+int columnas;
+int filas;
 
 	for (columnas = 0; columnas < 10; columnas++)
 	{
