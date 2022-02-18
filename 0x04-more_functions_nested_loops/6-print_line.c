45fec87b8091ae7ca3e8fa0bdbf44971a39071f2
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-18 17:26:02 -0500

    Se añadio task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: int inside the funtion
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
