31bfefee8b69cd7abba44363cac2bbe187072631
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-18 13:48:45 -0500

    Se subio todo para el task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - check the code.
+ *
+ * Ret
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
