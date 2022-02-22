82146f0db6decd5f26d197b7d0022267da72c030
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-21 21:55:02 -0500

    Se agrego el task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - check the code
+ *@s: a pointer inside the funtion.
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	char arr[100];
+	int i = 0;
+	int j;
+
+	for (j = 0; s[j]; j++)
+	{
+		arr[j] = s[j];
+	}
+	for (j--; j >= 0; j--)
+	{
+		s[j] = arr[i];
+		i++;
+	}
+}
