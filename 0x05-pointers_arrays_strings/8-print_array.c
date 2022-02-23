506695c461c10a94959df2ffca08b2034074f321
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-22 22:06:58 -0500

    Se creo task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,13 +6,13 @@
  * Return: Always 0.
  */
 
- void print_array(int *a, int n)
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		if(i != (n - 1))
+		if (i != (n - 1))
 		{
 			printf("%d, ", a[i]);
 		}
