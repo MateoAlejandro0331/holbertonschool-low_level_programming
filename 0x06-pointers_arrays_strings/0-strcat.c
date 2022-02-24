0501f3070ddc55808aa309fe939eb0a931353d83
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-24 10:50:35 -0500

    Se agrego betty al task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
+* _strcat - check the code
+*@dest: pointer inside the funtion
+*@src: pointer inside the funtion
 * Return: Always 0.
 */
 
 char *_strcat(char *dest, char *src)
 {
-	int i,j;
-	for (i = 0; dest[i]; i++);
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+		;
 	for (j = 0; src[j]; j++)
 	{
 		dest[i] = src[j];
