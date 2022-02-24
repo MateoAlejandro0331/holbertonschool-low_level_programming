9f15d3203c804a7214156c7962e96bace3cef79e
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-24 10:53:31 -0500

    Se agrego correccion de task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; dest[i]; i++)
 		;
-	for (j = 0; j <= n && src[j]; j++)
+	for (j = 0; j < n && src[j]; j++)
 	{
 		dest[i] = src[j];
 		i++;
