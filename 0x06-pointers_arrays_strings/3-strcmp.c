8d02bd07a5a8f93fc2274a4e5c9dd3fcef1dd13a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-24 16:45:10 -0500

    Se agrego el task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strcmp - check the code
+*@s1: pointer inside the funtion.
+*@s2: pointer inside the funtion.
+* Return: Always 0.
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int lens1,lens2;
+
+	for (lens1 = 0; s1[lens1]; lens1++);
+	for (lens2 = 0; s2[lens2]; lens2++);
+	if (lens1 == lens2)
+	{
+		return(0);
+	}
+	else if (lens1 < lens2)
+	{
+		return (-15);
+	}
+	else if (lens1 > lens2)
+	{
+		return (15);
+	}
+	return (0);
+}
