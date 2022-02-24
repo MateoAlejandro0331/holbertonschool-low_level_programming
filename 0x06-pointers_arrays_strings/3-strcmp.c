d7fb4c55f6a445d1712dced855cebc73eb1a2c4b
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-24 16:49:11 -0500

    Se agrego betty al task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int lens1,lens2;
+	int lens1, lens2;
 
-	for (lens1 = 0; s1[lens1]; lens1++);
-	for (lens2 = 0; s2[lens2]; lens2++);
+	for (lens1 = 0; s1[lens1]; lens1++)
+		;
+	for (lens2 = 0; s2[lens2]; lens2++)
+		;
 	if (lens1 == lens2)
 	{
-		return(0);
+		return (0);
 	}
 	else if (lens1 < lens2)
 	{
