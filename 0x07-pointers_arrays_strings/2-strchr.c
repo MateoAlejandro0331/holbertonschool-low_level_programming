1d478fa37d7a95c623c9e90e35ab53d8f85ae2c0
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-28 16:31:01 -0500

    Se subio el task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - check the code
+ *@s: pointer inside the funtion.
+ *@c: char inside the funtion.
+ * Return: Always 0.
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
