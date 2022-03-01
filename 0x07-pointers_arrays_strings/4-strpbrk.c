323b8d7d3d97a33f6fc2f89b434222e2178f3ef1
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-02-28 19:42:22 -0500

    Se agrego el task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - check the code
+ *@s: pointer inside the funtion.
+ *@accept: pointer inside the funtion.
+ * Return: Always 0.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (s);
+}
