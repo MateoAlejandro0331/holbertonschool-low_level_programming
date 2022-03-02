782be64768e307ab0fda410ac8e94f7bc6e7fc84
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-02 14:31:40 -0500

    Se agrego el task 0

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the code
+ *@s: pointer inside the funtion
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (!s || !*s)
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+
+}
