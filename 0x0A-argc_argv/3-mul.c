bf26c5c1d5d355fcf081b7532a6736e0422be421
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-04 15:47:55 -0500

    Se agrego el task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@argc: arguments of the funtion
+ *@argv: argumentes of the funtion
+ *Return: always cero.
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
