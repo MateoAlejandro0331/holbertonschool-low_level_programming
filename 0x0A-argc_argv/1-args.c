c0b2494f4f761a45a60d4a76991e6ae61b585a24
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-04 13:01:12 -0500

    Se agrego task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@argc: arguments of the funtion
+ *@argv: argumentes of the funtion
+ *Return: always cero.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
