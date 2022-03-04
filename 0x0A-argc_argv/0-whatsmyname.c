a011390fb4dd9eb5bbfeb8bf01c196419ff43d0d
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-04 12:24:14 -0500

    Se agrego el task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return - always cero
+ */
+
+int main (int argc, char * argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
