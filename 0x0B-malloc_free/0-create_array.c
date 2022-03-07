d066a2f4054dd87fce927ab435a2392b87f9342a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-07 18:32:02 -0500

    Se agrego el task 0 con betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * create_array - create an array during the runtime
+ * @size: the address of memory to print
+ * @c: type of date that is inside the array
  *
- * Return: Nothing.
+ * Return: the pointer that contains the array
  */
 
 char *create_array(unsigned int size, char c)
