97b3f20c093cf9deafa1d577fef1403dea256ca0
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-08 11:51:52 -0500

    Se agrego el task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,32 +10,63 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, x, z = 0;
+        int i, j, x, z = 0;
 	char *str;
 
-	if (s1 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 	else
 	{
-		for (i = 0; s1[i]; i++)
-			;
-		for (j = 0; s2[j]; j++)
-			;
-		j++;
+		if (s2 == NULL)
+		{
+			for (i = 0; s1[i]; i++)
+				;
+			i++;
 
-		str = malloc(sizeof(char) * (i + j));
-		if (!str)
-			return (NULL);
-		for (x = 0; x < i; x++)
-		{
-			str[x] = s1[x];
+			str = malloc(sizeof(char) * i);
+			if (!str)
+				return (NULL);
+			for (x = 0; x < i; x++)
+			{
+				str[x] = s1[x];
+			}
 		}
-		for (; x < (i + j); x++)
+		else if (s1 == NULL)
 		{
-			str[x] = s2[z];
-			z++;
+			for (i = 0; s2[i]; i++)
+				;
+			i++;
+
+			str = malloc(sizeof(char) * i);
+			if (!str)
+				return (NULL);
+			for (x = 0; x < i; x++)
+			{
+				str[x] = s2[x];
+			}
+		}
+		else
+		{
+			for (i = 0; s1[i]; i++)
+				;
+			for (j = 0; s2[j]; j++)
+				;
+			j++;
+
+			str = malloc(sizeof(char) * (i + j));
+			if (!str)
+				return (NULL);
+			for (x = 0; x < i; x++)
+			{
+				str[x] = s1[x];
+			}
+			for (; x < (i + j); x++)
+			{
+				str[x] = s2[z];
+				z++;
+			}
 		}
 	}
 	return (str);
