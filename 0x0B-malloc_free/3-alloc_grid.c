3332d322396d297d9762da3dd0fd37d4ca827da7
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-08 16:36:32 -0500

    Se agrego task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create a bidimensional array of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: doble pointer.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * (width * height));
+	if (!grid)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+			free(grid[i]);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
