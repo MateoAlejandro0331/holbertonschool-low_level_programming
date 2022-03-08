706ed58ad5d6d972b157592505fd5e1e49b05684
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-08 16:39:03 -0500

    Se agrego task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **grid;
 
-	if (width < 0 || height < 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc(sizeof(int *) * (width * height));
 	if (!grid)
