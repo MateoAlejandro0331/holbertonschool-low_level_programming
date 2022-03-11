fb613cb9e3bb1c4ea2bdc5bcea56626e08b8cfa4
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-11 10:43:35 -0500

    Se agrego task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - check the code
+ *@s: pointer inside the funtion
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int len1;
+
+	len1 = 0;
+	while (*s != '\0')
+	{
+		len1++;
+		s++;
+	}
+	return (len1);
+}
+/**
+ * string_nconcat - check the code
+ *@s1: pointer to concat
+ *@s2: pointer to concat
+ *@n: bytes of s2
+ * Return: pointer concat
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int size1, size2, i, j;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	if (n >= size2)
+	{
+		concat = malloc(sizeof(char) * (size1 + size2 + 1));
+		if (concat == NULL)
+			return (NULL);
+	}
+	else
+	{
+		concat = malloc(sizeof(char) * (size1 + n + 1));
+		if (concat == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < size1; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		concat[i] = s2[j];
+		i++;
+	}
+	concat[i] = '\0';
+	return (concat);
+
+}
