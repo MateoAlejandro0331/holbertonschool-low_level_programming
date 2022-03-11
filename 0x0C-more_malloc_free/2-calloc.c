eab751850b5b9d49ee05d22f99e4e1435466dda0
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-11 18:27:29 -0500

    Se agrego el task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	int *a;
+	char *a;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		a[i] = 0;
 	return (a);
 }
