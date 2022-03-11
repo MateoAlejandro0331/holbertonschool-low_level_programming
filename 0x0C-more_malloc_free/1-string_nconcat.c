fdea6dffbcfcdd9cd2f16554b214bb54ccba7beb
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-11 10:51:43 -0500

    Se agrego task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,6 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	size2 = _strlen(s2);
 	if (n >= size2)
 	{
+		n = size2;
 		concat = malloc(sizeof(char) * (size1 + size2 + 1));
 		if (concat == NULL)
 		{
