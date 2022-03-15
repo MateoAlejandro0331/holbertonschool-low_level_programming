7fca566b180eeaef4149e5f8dced71dfc9225664
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-14 19:12:12 -0500

    Se agrego el readme y el task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct to allocate diferentes values, and data types
+ *@name: Dogs name
+ *@age: Dogs age
+ *@owner: Owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
