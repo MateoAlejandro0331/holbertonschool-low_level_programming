3d274b5e65e8e2987cab909711dc0db2e3578f51
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-14 20:53:51 -0500

    Se agrego el task 1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,9 +2,9 @@
 #include "dog.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_dog - check the code
+ *@d: pointer to the struct
+ * Return: Nothing
  */
 
 void print_dog(struct dog *d)
