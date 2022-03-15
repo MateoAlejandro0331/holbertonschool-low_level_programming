ec025a8606889eaef2f9d7c316ca965f292e0124
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-14 19:33:41 -0500

    Se agrego el task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - check the code
+ *@d: pointer to the direction of the struct
+ *@name: Dogs name
+ *@age: Dogs age
+ *@owner: Owner name
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
