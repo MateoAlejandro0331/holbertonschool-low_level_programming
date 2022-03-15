130c93967d821098e57f8c6d74cff715c912b77b
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-14 21:17:44 -0500

    Se agrego el task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - New type of struct dog 
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
