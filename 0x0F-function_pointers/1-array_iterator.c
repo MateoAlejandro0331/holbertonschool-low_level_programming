c8aaa756481d37818df2f412f580e9a35ea946e3
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-17 11:01:45 -0500

    Se agrego task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - prints an integer
+ * @array: array of integers to show
+ * @size: size of the array
+ * @action: pointer to the funtion
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
