34f9ba73176acd234cc1d9c43ce8cd67abdbf0d8
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-17 12:13:54 -0500

    Se agrego el task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (cmp)
-	{
-		for (i = 0; i < size; i++)
-		{	
-			cmp(array[i]);
-				if (cmp(array[i]) == 1)
+	for (i = 0; i < size; i++)
+	{	
+		cmp(array[i]);
+			if (cmp(array[i]) == 1)
 				return (i);
-		}
-		return (-1);
 	}
+	return (-1);
 }
