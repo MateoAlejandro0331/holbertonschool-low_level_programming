dbb0554d10c3de8badc4f32b45098f3bbb93f54b
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-17 12:06:26 -0500

    Se agrego el task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - Funtion that get the address of another funtion
+ * @array: array to compare integers
+ * @cmp: address of the funtion
+ * @size: size of the array
+ * Return: Value determinate
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
+}
