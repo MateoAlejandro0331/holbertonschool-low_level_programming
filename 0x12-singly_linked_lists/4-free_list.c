36df6df1f9862b07f9ccdc7250658566ea1afb4a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-29 15:10:47 -0500

    add task4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,19 @@
 #include "lists.h"
-
+/**
+ * free_list - free the memory in the heap for de linked list
+ * @head: pointer to the firts node of the list
+ * Return: Nothing
+ * its necesary to free all the arrays inside the list
+ */
 void free_list(list_t *head)
 {
-    if (head)
-    {
-        free_list(head->next);
-    }
-    free(head);
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
