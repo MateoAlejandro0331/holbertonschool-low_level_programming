28c6dfb125ce778cc7d9488c6c59f767c9c5490c
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-03-29 12:13:52 -0500

    add task3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	/*New node at the end of the list*/
+	list_t *new_node;
+	list_t *last = *head; /*pointer to traverse the list till last node*/
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	/* Else traverse till the last node */
+	while (last->next != NULL)
+		last = last->next;
+	/*Change the next of last node */
+	last->next = new_node;
+	return (new_node);
+}
