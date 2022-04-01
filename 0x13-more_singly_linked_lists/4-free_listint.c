2e833ba55fc449ba0db11a3f3799acd6d1421eb2
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-01 11:03:02 -0500

    Se añañadio task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,17 @@
 #include "lists.h"
-
+/**
+ * free_listint - check the code
+ * @head: pointer to the firts node
+ * Return: nothing.
+ */
 void free_listint(listint_t *head)
 {
-    listint_t *aux;
+	listint_t *aux;
 
-    while (head)
-    {
-        aux = head;
-        head = head->next;
-        free(aux);
-    }
+	while (head)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
 }
