8e4954964651e54023d6a0d343cd73fd60bb4d7d
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-01 18:15:40 -0500

    Se añañadio task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *aux1 = *head;
 	listint_t *aux2 = *head;
-	listint_t *aux_count = *head;
 	unsigned int count;
 
 	if (!*head)
@@ -19,14 +18,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	for (count = 0; aux_count; count++)
-		aux_count = aux_count->next;
-	if (idx < count)
-		return (NULL);
+
 	for (count = 1; count < idx; count++)
+	{
 		aux1 = aux1->next;
+		if (!aux1)
+			return (NULL);
+	}
 	for (count = 1; count < (idx + 1); count++)
+	{
 		aux2 = aux2->next;
+		if (!aux2)
+			return (NULL);
+	}
 	new_node->n = n;
 	aux1->next = new_node;
 	new_node->next = aux2;
