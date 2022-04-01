107699667e664add929f16c1d3def419d8fb9a1f
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-01 12:18:37 -0500

    Se añañadio task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - check the code
+ *  @head: pointer to the firts node
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux = *head;
+	listint_t *aux_free;
+
+	while (aux)
+	{
+		aux_free = aux;
+		aux = aux->next;
+		free(aux_free);
+	}
+	*head = NULL;
+}
