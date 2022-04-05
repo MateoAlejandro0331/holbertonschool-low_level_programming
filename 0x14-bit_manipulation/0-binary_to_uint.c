1ad9c36c5dac1ac1d41dad7d8cca69b7fe2fa14c
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-04 21:51:15 -0500

    Se agrego task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,26 +6,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int pot[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048,
-						  4096, 8192, 16384, 32768, 65536, 131072, 262144,
-						  524288, 1048576, 2097152, 4194304, 8388608,
-						  16777216, 33554432, 67108864, 134217728,
-						  268435456, 536870912, 1073741824};
-	unsigned int i, len, num = 0;
+	unsigned int  num = 0;
+	int len, base = 1;
 
-	if (!b)
-		return (0);
+			 if (!b) return (0);
 	for (len = 0; b[len]; len++)
 		;
 	len--;
-	for (i = 0; b[i]; i++)
+	while (b[len])
 	{
-		if (b[i] == '1')
-		{
-			num += pot[len];
-		}
-		else if ((b[i] != '1') & (b[i] != '0'))
+		if (b[len] == '1')
+			num = num + base;
+		if ((b[len] != '1') && (b[len] != '0'))
 			return (0);
+		base = base * 2;
 		len--;
 	}
 	return (num);
