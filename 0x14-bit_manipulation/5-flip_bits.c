e27275acd16280d1daca967225fcedb8eb025af0
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-05 18:01:41 -0500

    se agrego task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,12 +7,12 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int a_xor_m = n ^ m;
+	unsigned long int n_xor_m = n ^ m;
 	int count = 0;
 
-	while (a_xor_m > 0)
+	while (n_xor_m > 0)
 	{
-		a_xor_m = a_xor_m & (a_xor_m - 1);
+		n_xor_m = n_xor_m & (n_xor_m - 1);
 		count++;
 	}
 	return (count);
