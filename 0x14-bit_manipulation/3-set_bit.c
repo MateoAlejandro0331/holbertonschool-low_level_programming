35d96b8094e443cbe1d8c5b355242de2f93fa966
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-05 14:16:15 -0500

    se agrego task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * set_bit - set a number bit
+ * @n: number to extract the index
+ * @index: index to the number
+ * Return: set number.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bits = 64;
+	int num = 1;
+
+	num = num << index;
+	if (index <= bits)
+	{
+
+		*n = (num | *n);
+	}
+	else
+		return (-1);
+	return (*n);
+}
