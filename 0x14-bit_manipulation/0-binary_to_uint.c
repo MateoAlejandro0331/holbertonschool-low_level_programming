d67b930aea721b1b0864b7002bc1ee4b6d487c91
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-04 21:52:27 -0500

    Se agrego task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int  num = 0;
 	int len, base = 1;
 
-			 if (!b) return (0);
+	if (!b)
+		return (0);
 	for (len = 0; b[len]; len++)
 		;
 	len--;
