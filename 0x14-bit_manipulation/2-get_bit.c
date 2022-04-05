421838d431134e6e3f747d130701f3639de66bcc
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-05 10:58:49 -0500

    se agrego task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - get bit index
+ * @n: number to extract the index
+ * @index: index to the number
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int num = n;
+
+	if (num)
+	{
+		num = num >> index;
+		return (num & 1);
+	}
+	else
+		return (-1);
+}
