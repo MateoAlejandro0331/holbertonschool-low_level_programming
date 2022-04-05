4dbdf85f87b35a4da32f0dcbb78f64bc9755633b
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-04 19:00:04 -0500

    Se agrego task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint - check the code
+ * @b: binary to convert
+ * Return: num decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int pot[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+	unsigned int i, len, num = 0;
+
+	for (len = 0; b[len]; len++)
+		;
+	len--;
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] == '1')
+		{
+			num += pot[len];
+		}
+		len--;
+	}
+	return (num);
+}
