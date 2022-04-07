3e69af4d13c35c897d449fed17d3719a5a6e7977
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-06 23:08:27 -0500

    Se agrego task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * append_text_to_file - append text to file
+ * @filename: filename
+ * @text_content: content of the file
+ * Return: 1 or -1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd = 0, wr = 0, i;
