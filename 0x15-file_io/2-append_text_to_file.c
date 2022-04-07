82d19cf8e1bbf2a619612432e1dfe664a52f8cc3
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-06 23:06:30 -0500

    Se agrego task2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = 0, wr = 0, i;
+
+	if (!text_content)
+		text_content = "";
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+			;
+		wr = write(fd, text_content, i);
+		if (wr < 0)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
