ff1b6b0f44e3ffb166a19ebac8fe904427828a2d
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-04-07 23:00:34 -0500

    Se agrego task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac > 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit (97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 < 0)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
@@ -19,6 +19,8 @@ int main(int ac, char **av)
 	if (fd2 < 0)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	rd = read(fd1, buf, 1024);
+	if (rd < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	while (rd > 0)
 	{
 		wr = write(fd2, buf, rd);
