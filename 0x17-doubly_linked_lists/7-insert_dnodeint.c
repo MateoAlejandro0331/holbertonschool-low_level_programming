ed131860e21129e646cece91d1e5a4ef9bd34f8c
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-12 18:27:07 -0500

    Adding task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * len_list - return the number of elements of a linked list
+ * @h: poiinter to the first node in the list
+ * Return: number of elements
+ *
+ */
+unsigned int len_list(dlistint_t *h)
+{
+	dlistint_t *current = h;
+	unsigned int count = 0;
+
+	while (current)
+	{
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
+/**
  * insert_dnodeint_at_index - add node at some index
  * @h: pointer to the head:
  * @idx: index
@@ -10,26 +28,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current = NULL;
 	dlistint_t *new = NULL;
-	unsigned int index = 0;
+	unsigned int index = 0, len = 0;
 
-	if (h == NULL)
+	if (!h)
 		return (NULL);
 	current = *h;
-	if (idx == 0)
-	{
-		new = malloc(sizeof(dlistint_t));
-		if (!new)
-			return (NULL);
-		new->n = n;
-		new->next = current;
-		new->prev = current->prev;
-		current->prev = new;
-		(*h) = new;
-		return (new);
-	}
+	len = len_list(current);
+	/**printf("%d", len);*/
+	if (idx == len)
+		return (add_dnodeint_end(h, n));
 	while (current)
 	{
-		if (index == idx)
+		if (idx == index)
 		{
 			new = malloc(sizeof(dlistint_t));
 			if (!new)
@@ -37,8 +47,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->n = n;
 			new->next = current;
 			new->prev = current->prev;
-			(current->prev)->next = new;
-			current->prev = new;
+			if (index == 0)
+			{
+				current->prev = new;
+				(*h) = new;
+				return (new);
+			}
+			else
+			{
+				(current->prev)->next = new;
+				current->prev = new;
+			}
 			return (new);
 		}
 		current = current->next;
