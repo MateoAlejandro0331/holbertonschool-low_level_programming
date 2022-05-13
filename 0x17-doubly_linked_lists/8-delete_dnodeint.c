36f9141e612de94c643c7b8f8dd0245c9a13424a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-12 21:53:04 -0500

    Adding task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ *
+ * 
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current = NULL;
+    unsigned int idx = 0;
+
+    if (!*head || !head)
+        return (-1);
+    current = *head;
+    while (idx < index)
+    {
+        if (!current)
+            return (-1);
+        current = current->next;
+        idx++;
+    }
+    if (*head == current)
+        *head = current->next;
+    if (current->next)
+        (current->next)->prev = current->prev;
+    if (current->prev)
+        (current->prev)->next = current->next;
+    free(current);
+    return (1);
+}
