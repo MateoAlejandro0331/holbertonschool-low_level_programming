df3e23b5f26cabb19218823bd1d1270c3157d297
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-16 17:44:40 -0500

    Estoy tostado

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -3,5 +3,4 @@ touch libdynamic.so
 chmod 0775 libdynamic.so
 gcc -Wall -fPIC -c *.c
 gcc -shared -o libdynamic.so *.o
-nm -D libholberton.so
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+nm -D libdymanic.so
