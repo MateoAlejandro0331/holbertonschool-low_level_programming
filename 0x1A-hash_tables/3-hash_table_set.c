e60f06f129a7c68b8587e5ef7cdc3034816b6c88
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-05 23:19:37 -0500

    Adding changes to task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node = NULL;
 
+	if (!ht)
+		return (0);
 	if (!key)
 		return (0);
 	if (!value)
