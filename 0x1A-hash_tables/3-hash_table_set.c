40cc1b1caaca1b62b117e4ae5f1dac2c06e2e0e6
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-05 22:05:02 -0500

    Adding task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function to set the values
+ * @ht: pointer to the table
+ * @key: key to store and find
+ * @value: value to store
+ * Return: 1 if succes or 0 if fail
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node = NULL;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	index = key_index(key, ht->size);
+	if (ht->array[index] == NULL)
+	{
+		node->key = strdup(key);
+		node->value = strdup(value);
+		node->next = NULL;
+		ht->array[index] = node;
+		return (1);
+	}
+	else
+	{
+		node->key = strdup(key);
+		node->value = strdup(value);
+		node->next = ht->array[index];
+		ht->array[index] = node;
+		return (1);
+	}
+	return (0);
+}
