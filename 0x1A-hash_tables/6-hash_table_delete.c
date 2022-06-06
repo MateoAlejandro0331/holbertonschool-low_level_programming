c9f14d5f8de4b69695db7e889172c76e4a51f207
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-06 18:06:50 -0500

    Adding task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -22,16 +22,14 @@ void free_recursion(hash_node_t *aux)
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *aux = NULL;
 	unsigned long int i = 0;
 
 	if (!ht)
 		return;
 	for (; i < ht->size; i++)
 	{
-		aux = ht->array[i];
-		if (aux)
-			free_recursion(aux);
+		if (ht->array[i])
+			free_recursion(ht->array[i]);
 	}
 	free(ht->array);
 	free(ht);
