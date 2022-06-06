cfcb4b6bf4b01faa4fe2f3fc77d2200b20b1ff71
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-05 22:17:24 -0500

    Adding changes to task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,8 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
-		return (NULL);
-	index = key_index(key, ht->size);
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
 		node->key = strdup(key);
